d8dd339101373b629e1e0c63f795fb2de942011f
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-11 15:25:00 +0600

    dom project

--- Dom Practice1/script.js
+++ Dom Practice1/script.js
@@ -5,15 +5,18 @@ let list = document.querySelector("ul");
 
 btn.addEventListener("click", () => {
   const inputValue = number.value;
-  if (Boolean(inputValue - 10)) {
+  if (Boolean(inputValue - 10) == false) {
     list.innerHTML = "";
+    const error2 = "String value is not possible";
+    heading.innerHTML = error2;
+  } else if (inputValue === "") {
+    list.innerHTML = "";
+    const error1 = "Empty value is not possible";
+    heading.innerHTML = error1;
+  } else {
     heading.innerHTML = "";
     for (let i = 1; i <= 10; i++) {
       list.innerHTML += `<li>${inputValue} * ${i} = ${inputValue * i}</li>`;
     }
-  } else {
-    list.innerHTML = "";
-    const error = "String value is not possible";
-    heading.innerHTML = error;
   }
 });
