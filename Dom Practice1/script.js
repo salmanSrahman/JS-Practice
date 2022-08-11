2900e12e22dbcaec4bbc5310211884a48cfa8428
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-11 12:27:07 +0600

    dom project

--- Dom Practice1/script.js
+++ Dom Practice1/script.js
@@ -1,13 +1,19 @@
 let number = document.querySelector(".number");
 let btn = document.querySelector(".btn");
 let heading = document.querySelector("h3");
+let list = document.querySelector("ul");
 
 btn.addEventListener("click", () => {
   const inputValue = number.value;
-  console.log(inputValue - 10);
-
-  //   for (let i = 1; i <= 10; i++) {
-  //     console.log(inputValue + "*" + i + "=" + inputValue * i);
-
-  //   }
+  if (Boolean(inputValue - 10)) {
+    list.innerHTML = "";
+    heading.innerHTML = "";
+    for (let i = 1; i <= 10; i++) {
+      list.innerHTML += `<li>${inputValue} * ${i} = ${inputValue * i}</li>`;
+    }
+  } else {
+    list.innerHTML = "";
+    const error = "String value is not possible";
+    heading.innerHTML = error;
+  }
 });
